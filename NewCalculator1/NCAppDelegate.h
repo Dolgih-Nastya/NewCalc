5a5fbb5391447f182f0dc2c9eb365e1905e39599
Анастасия Долгих <anastasiadolgih@Anastasias-Mac.local>
2014-04-23 00:09:04 -0700

    Initial Commit

---
+++ NewCalculator1/NCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NCAppDelegate.h
+//  NewCalculator1
+//
+//  Created by Анастасия Долгих on 4/23/14.
+//  Copyright (c) 2014 Anastasia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NewCalculator1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NewCalculator1
+//
+//  Created by Анастасия Долгих on 4/23/14.
+//  Copyright (c) 2014 Anastasia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NCAppDelegate class]));
+    }
+}
